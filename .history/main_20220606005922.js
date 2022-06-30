5e4b6d5cc295f4d9a196bd87f20f4eeaa6fbaf41
mosak <mosak0217@gmail.com>
2022-06-30 12:40:22 +0900

    first commit

---
+++ .history/main_20220606005922.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// href=”#”のaタグを取得（→その後、href属性からid名を抜き出す）
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+// for文を回して、aタグそれぞれに対してクリックイベント
+  for (let i = 0; i < smoothScrollTrigger.length; i++){
+    smoothScrollTrigger[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      let href = smoothScrollTrigger[i].getAttribute('href');
+      let targetElement = document.getElementById(href.replace('#', ''));
+      // ターゲットの位置を取得
+      const rect = targetElement.getBoundingClientRect().top;
+      const offset = window.pageYOffset;
+      const gap = 60;
+      const target = rect + offset - gap;
+      
+      window.scrollTo({
+        top: target,
+        behavior: 'smooth',
+      });
+    });
+  }
---
+++ .history/main_20220606111425.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// href=”#”のaタグを取得（→その後、href属性からid名を抜き出す）
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+// for文を回して、aタグそれぞれに対してクリックイベント
+
+// -- 繰り返し処理 1 回目 --
+// 1) 初期化式で変数 i に 1 が代入される
+// 2) 条件式 i < 3 が true
+// 3) console.log("i = " + i); が実行される
+// 4) 変化式 i++ が実行されて変数 i の値が 2 となる
+  for (let i = 0; i < smoothScrollTrigger.length; i++){
+    smoothScrollTrigger[i].addEventListener('click', (e) => {
+      e.preventDefault();
+      let href = smoothScrollTrigger[i].getAttribute('href');
+      let targetElement = document.getElementById(href.replace('#', ''));
+      // ターゲットの位置を取得
+      const rect = targetElement.getBoundingClientRect().top;
+      const offset = window.pageYOffset;
+      const gap = 60;
+      const target = rect + offset - gap;
+      // スムースにスクロールする
+      window.scrollTo({
+        top: target,
+        behavior: 'smooth',
+      });
+    });
+  }
